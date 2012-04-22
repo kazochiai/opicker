3068817502a456abacb66d180593c2cf521c9522
Kazuhito Ochiai <kazuhito0000@hotmail.com>
2012-04-21 23:34:55 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Opicker::Application.config.secret_token = 'f6a0b1473416d4ad02291ec5fa45faa35d278bf74bf6baf0d5055d84278e2fc7afd3ad09d2f0067cf75bd1ef884c09d66fbece791776ba495e079cf15efb60b5'
